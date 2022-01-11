b11b3ec2bbe31a585511ba32b9f2b9b2aed05db0
Kevin Lacson <lacsonky@gmail.com>
2022-01-10 23:46:18 -0500

    renamed build profile to generateMarkup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { initBuild, team } = require("./src/inquire");
-const generateMarkup = require("./src/buildProfile");
+const generateMarkup = require("./src/generateMarkup");
 
-async function build() {
+async function createProfile() {
     await initBuild();
     const markUp = await generateMarkup(team);
     console.log(markUp);
 }
 
-build();
+createProfile();
